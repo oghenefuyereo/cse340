3efb748f33770de44c120c11ff799eb9f1f160cf
oghenefuyereo <oghenefuyereo@gmail.com>
2024-11-11 19:53:15 -0500

    initial

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const static = require("./routes/static");
+const staticRoutes = require("./routes/static"); // Renamed to clarify it serves static files
 const expressLayouts = require("express-ejs-layouts");
 
 /* ***********************
@@ -20,25 +20,33 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // Not at views root
 
 /* ***********************
+ * Middleware for Static Files
+ *************************/
+app.use(express.static("public"));  // This serves static files from the "public" folder
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/images", express.static(__dirname + "/public/images"));
+
+/* ***********************
  * Routes
  *************************/
-app.use(static);
+app.use(staticRoutes); // Serving static routes, ensure staticRoutes is defined correctly
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
+const port = process.env.PORT || 5500; // Default to 5500 if PORT is not provided
 const host = process.env.HOST || "localhost"; // Default to localhost if HOST is undefined
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`App listening on ${host}:${port}`);
+  console.log(`App listening on http://${host}:${port}`);
 });
 
-//Index route
+// Index route
 app.get("/", function (req, res) {
   res.render("index", { title: "Home" });
 });
