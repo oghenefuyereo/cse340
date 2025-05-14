8ababee0e98becd4563d7777d9ba56914599bca4
oghenefuyereo <oghenefuyereo@gmail.com>
2025-05-14 15:09:02 -0400

    update

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,9 @@
-const express = require('express');
+// routes/static.js
+const express = require("express");
 const router = express.Router();
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About Us" });
+});
 
 module.exports = router;
-
-
-
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
@@ -9,22 +10,27 @@ const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const env = require("dotenv").config();
 const app = express();
-const static = require("./routes/static");
+const staticRoutes = require("./routes/static"); // Ensure this file exists
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // not at views root
+app.set("layout", "./layouts/layout"); // Layout file in the 'layouts' folder
+
+/* ***********************
+ * Middleware for Static Files
+ *************************/
+app.use(express.static("public")); // Serve static files from 'public' folder
 
 /* ***********************
  * Routes
  *************************/
-app.use(static);
+app.use(staticRoutes); // This is for any custom routes you may have in static.js
 
-//Index route
-app.get("/", function(req, res){
+// Index route
+app.get("/", function (req, res) {
   res.render("index", { title: "Home" });
 });
 
@@ -32,12 +38,22 @@ app.get("/", function(req, res){
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT || 3000;  // Default to 3000 if not provided
+const host = process.env.HOST || "localhost";  // Default to 'localhost' if not provided
+
+// Debug environment variables
+console.log("PORT:", port);
+console.log("HOST:", host);
+
+// Debug views directory
+console.log("Views directory:", app.get("views"));
+
+// Debug middleware
+console.log("Static middleware loaded: Serving static files from './public'");
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
+  console.log(`App listening on http://${host}:${port}`);
 });
