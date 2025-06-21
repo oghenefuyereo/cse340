dfd25f3c44957d34d858dc6247970a7715bf79f3
oghenefuyereo <oghenefuyereo@gmail.com>
2025-06-21 11:54:10 -0400

    update

--- database/database_update.sql
+++ database/database_update.sql
@@ -6,3 +6,23 @@ UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
 
+    
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/vehicles/vehicles/', '/vehicles/')
+WHERE inv_image LIKE '%/vehicles/vehicles/%';
+
+UPDATE inventory
+SET inv_thumbnail = REPLACE(inv_thumbnail, '/vehicles/vehicles/', '/vehicles/')
+WHERE inv_thumbnail LIKE '%/vehicles/vehicles/%';
+
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/')
+WHERE inv_image LIKE '/images/%'
+  AND inv_image NOT LIKE '/images/vehicles/%';
+
+UPDATE inventory
+SET inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+WHERE inv_thumbnail LIKE '/images/%'
+  AND inv_thumbnail NOT LIKE '/images/vehicles/%';
+
